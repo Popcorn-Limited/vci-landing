92df542eeaeda8b36b4bbbda6dbaaee2d72206b6
RedVeil <leon.niesler@gmail.com>
2023-04-21 14:11:56 +0200

    fixed build issue

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -33,19 +33,19 @@ const Carousel = () => {
       content:
         "Zokyo is an end-to-end security resource that provides distinguishable security auditing and penetration testing services alongside prominent vulnerability assessments.",
       color: "bg-[#FA5A6E]",
-      image:"blocksec-logo.png"
+      image: "blocksec-logo.png"
     },
     {
       title: "Code4rena",
       content: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.",
       color: "bg-[#9B55FF]",
-      image:"c4-logo.png"
+      image: "c4-logo.png"
     },
     {
       title: `Immunefi`,
       content: `Immunefi is the leading bug bounty platform for Web3.`,
       color: "bg-[#05BE64]",
-      image:"immunifi-logo.png"
+      image: "immunifi-logo.png"
     },
   ];
 
@@ -54,22 +54,19 @@ const Carousel = () => {
       <div className="">
         <Slider {...settings} ref={(slider) => (customSlider.current = slider)}>
           {tutorialSteps.map((step, index) => (
-            <div>
-              <div className="px-2 flex flex-col justify-between">
-                <div>
-                  <div className={`w-full aspect-square flex flex-col justify-center items-center rounded-lg mb-6 ${step.color}`}>
-                    <img src={`/images/icons/${step.image}`} className='' />
-                  </div>
-                  <p className="text-4xl mb-4">{step.title}</p>
-                  <p className="text-xl">{step.content}</p>
-                </div>
+            <div key={step.title} className="px-2 flex flex-col justify-between">
+              <div className={`w-full aspect-square flex flex-col justify-center items-center rounded-lg mb-6 ${step.color}`}>
+                <img src={`/images/icons/${step.image}`} className='' />
               </div>
+              <p className="text-4xl mb-4">{step.title}</p>
+              <p className="text-xl">{step.content}</p>
             </div>
           ))}
         </Slider>
         <div className="flex justify-center pt-6 gap-5">
-          {tutorialSteps.map((steps, index) => (
+          {tutorialSteps.map((step, index) => (
             <div
+              key={`${step.title}-button`}
               className={`h-5 w-5 rounded-full mx-1 border-2 border-black ${currentSlide === index ? "bg-black" : ""} `}
               onClick={() => gotoSlide(index)}
             ></div>
