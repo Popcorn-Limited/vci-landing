11bfddeade7f7a8af9d71b4b0829ff5c55bbb633
amatureApe <amatureApe@gmail.com>
2023-10-04 03:19:26 -0700

    styling cleanup

--- components/Sections/GetStartedSection.tsx
+++ components/Sections/GetStartedSection.tsx
@@ -5,6 +5,8 @@ import AppButton from "@/components/Common/AppButton";
 export default function GetStartedSection() {
     const sectionRef = useRef(null);
     const [startTyping, setStartTyping] = useState(false);
+    const [typingDone, setTypingDone] = useState(false);
+    const [opacity, setOpacity] = useState(0);
 
     useEffect(() => {
         const observer = new IntersectionObserver(entries => {
@@ -23,11 +25,19 @@ export default function GetStartedSection() {
         return () => observer.disconnect();
     }, []);
 
+    useEffect(() => {
+        if (typingDone) {
+            setTimeout(() => {
+                setOpacity(1);
+            }, 0);
+        }
+    }, [typingDone]);
+
     return (
         <div ref={sectionRef} className={`min-h-[1100px] bg-gradient-to-b from-black to-[#030E22] pt-60 smmd:pl-8 smmd:pr-0 flex flex-col smmd:flex-row smmd:gap-5 gap-16`}>
             <div className={`flex flex-col smmd:pb-5 px-6 items-start z-10`}>
                 {startTyping && (
-                    <Typist cursor={{ show: false }} avgTypingDelay={12.5}>
+                    <Typist cursor={{ show: false }} avgTypingDelay={12.5} onTypingDone={() => setTypingDone(true)}>
                         <div className={`text-[40px] smmd:text-[56px] max-w-[740px] leading-none`}>
                             <span className={`opacity-60`}>The</span>
                             <span className={`italic text-[#7AFB79] font-georgia opacity-100`}> no code toolkit </span>
@@ -42,11 +52,13 @@ export default function GetStartedSection() {
                         </div>
                     </Typist>
                 )}
-                <div className={`smmd:pl-[200px]`}>
-                    <a href="https://docs.pop.network/products/vaultcraft">
-                        <AppButton className={`font-bold w-full max-w-[176px]`} text={'Get Started'} />
-                    </a>
-                </div>
+                {typingDone && (
+                    <div style={{ opacity: opacity, transition: 'opacity 1000ms ease-in-out' }} className={`smmd:pl-[200px]`}>
+                        <a href="https://docs.pop.network/products/vaultcraft">
+                            <AppButton className={`font-bold w-full max-w-[176px]`} text={'Get Started'} />
+                        </a>
+                    </div>
+                )}
             </div>
             <img src="/images/GetStartedSectionBg.png" className={`absolute right-0 max-w-[1300px] top-[970px]`} />
         </div>
--- components/Sections/SDKSection.tsx
+++ components/Sections/SDKSection.tsx
@@ -5,6 +5,8 @@ import AppButton from "@/components/Common/AppButton";
 export default function SDKSection() {
     const sectionRef = useRef(null);
     const [startTyping, setStartTyping] = useState(false);
+    const [typingDone, setTypingDone] = useState(false);
+    const [opacity, setOpacity] = useState(0);
 
     useEffect(() => {
         const observer = new IntersectionObserver(entries => {
@@ -23,28 +25,40 @@ export default function SDKSection() {
         return () => observer.disconnect();
     }, []);
 
+    useEffect(() => {
+        if (typingDone) {
+            const timeoutId = setTimeout(() => {
+                setOpacity(1);
+            }, 100);
+            return () => clearTimeout(timeoutId);
+        }
+    }, [typingDone]);
+
     return (
         <div ref={sectionRef} className={`bg-[#030E22] py-[64px] smmd:py-[160px] px-[24px] smmd:px-[80px] flex flex-col smmd:flex-row-reverse gap-[48px] justify-between items-end min-h-[750px]`}>
             <div className={`height-full flex flex-col gap-8 smmd:w-[680px] z-[1]`}>
                 <p className={`font-georgia italic text-[#DFFF1C] font-[20px]`}>Vaultcraft SDK</p>
 
                 {startTyping && (
-                    <Typist cursor={{ show: false }} avgTypingDelay={12.5}>
+                    <Typist cursor={{ show: false }} avgTypingDelay={12.5} onTypingDone={() => setTypingDone(true)}>
                         <h2 className={`text-[40px] font-khTeka smmd:text-[56px] leading-none max-w-[655px]`}>
                             Build cross‑chain automated vault strategies with just a
                             <span className={`italic text-[#F289E6] font-georgia`}> few lines of code</span>
                         </h2>
                         <span className={`max-w-[500px] whitespace-pre-line`}>
-                            <p>Work seamlessly with any token and from any chain.</p>
-                            <p>Integrate the Vaulcraft widget with just a few lines of code.</p>
-                            <p>Looking for a completely custom UX? Use the Vaulcraft API & SDK instead.</p>
+                            <p className={`py-[12px] mt-[20px]`}>Work seamlessly with any token and from any chain.</p>
+                            <p className={`py-[12px]`}>Integrate the Vaulcraft widget with just a few lines of code.</p>
+                            <p className={`py-[12px]`}>Looking for a completely custom UX? Use the Vaulcraft API & SDK instead.</p>
                         </span>
                     </Typist>
                 )}
-
-                <a className={`self-start`} href="https://docs.pop.network/products/vaultcraft">
-                    <AppButton className={`font-bold w-full max-w-[176px]`} text={'Start Building'} />
-                </a>
+                {typingDone && (
+                    <div style={{ opacity: opacity, transition: 'opacity 1000ms ease-in-out' }} className={`self-start`}>
+                        <a href="https://docs.pop.network/products/vaultcraft">
+                            <AppButton className={`font-bold w-full max-w-[176px]`} text={'Start Building'} />
+                        </a>
+                    </div>
+                )}
             </div>
             <div className={`smmd:px-10 w-full smmd:w-auto`}>
                 <img className={`w-[420px] m-auto`} src="images/SDKSectionCode.png" alt="code" />
